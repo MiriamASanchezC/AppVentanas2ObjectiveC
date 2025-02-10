c65a11e55315528e1eeff06305f299d140bfcc2f
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-10 17:13:33 -0600

    Initial Commit

---
+++ AppVentanas2/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AppVentanas2
+//
+//  Created by Miriam Sanchez on 10/02/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AppVentanas2/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  AppVentanas2
+//
+//  Created by Miriam Sanchez on 10/02/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
